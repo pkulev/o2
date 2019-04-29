c9ecdab68b76e036fab6f1c741bdf01886130416
Pavel Kulyov <kulyov.pavel@gmail.com>
2019-04-29 09:38:04 +0300

    make ui widget work with camera and parent offsets
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- src/ui.lisp
+++ src/ui.lisp
@@ -10,7 +10,17 @@
                 :accessor data-getter)))
 
 (defmethod render ((w text-widget))
-  (with-slots (x y data-getter) w
-    (draw-text :ubuntu (funcall data-getter) x y :color '(255 255 255))))
+  (with-slots (parent x y data-getter) w
+    (format t "drawing '~a' in ~a:~a~&" (funcall data-getter) x y)
+    (if parent
+        (with-slots (camera) (current-app-state)
+          (with-slots ((parent-x x)
+                       (parent-y y)) parent
+            (format t "parent ~a:~a" parent-x parent-y)
+            (draw-text :ubuntu (funcall data-getter)
+                       (+ x parent-x (- (car camera)))
+                       (+ y parent-y (- (cdr camera))) :color '(255 255 255))))
+
+        (draw-text :ubuntu (funcall data-getter) x y :color '(255 255 255)))))
   
 
