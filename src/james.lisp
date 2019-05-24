6a8d5d7221b30806d9426f5b6e7a77693dfb09be
Ekaterina Vaartis <vaartis@cock.li>
2019-05-25 02:29:58 +0300

    Remove the old james definition

--- src/james.lisp
+++ src/james.lisp
@@ -1,49 +1,5 @@
 (in-package :o2)
 
-(defclass james (game-object)
-  ((render-priority :initform 2
-                    :allocation :class)
-   (health :initform 100
-           :reader health)
-   (max-health :initform 100
-               :reader max-health)
-   (armor :initform 100
-          :reader armor)
-
-   (weapon :initform nil
-           :accessor weapon)
-
-   (fire? :initform nil
-          :accessor fire?)
-
-   (collider :initform nil
-             :accessor collider)
-
-   ;; Jumping
-   (jumping? :initform nil
-             :accessor jumping?)
-   ;; Velocity added when jumping
-   (jumping-added-velocity :initform -10
-                           :accessor jumping-added-velocity)
-
-   ;; Position/Rotation of the sprite
-   (pos-direction :initform 1
-                  :accessor pos-direction)
-
-   (sitting? :initform nil
-             :accessor sitting?)
-   (sitting-sprite :initform nil
-                   :initarg :sitting-sprite
-                   :accessor sitting-sprite)
-
-   ;; unix-to-timestamp 0 is definetly less than any other time
-   (last-hit-time :initform (local-time:unix-to-timestamp 0)
-                  :accessor last-hit-time)
-
-   ;; amount of seconds when that the player is invincible to enemy damage
-   (invinc-seconds :initform 2
-                   :accessor invinc-second)))
-
 (defun make-james (&key (position (cons 0 0)) sprite sitting-sprite space)
   (let* ((dfloat-width (coerce (sprite-width :player) 'double-float))
          (dfloat-height (coerce (sprite-height :player) 'double-float))
@@ -58,7 +14,7 @@
                                      dfloat-height
                                      0d0)))
          (player (make-instance
-                  'james
+                  'game-object
                   :components (list
                                (make-instance 'player-tag)
                                (make-instance 'health-c
@@ -136,31 +92,3 @@
           (dfloat-y (coerce (cdr position) 'double-float)))
       (setf (chipmunk:position rigid-body) (chipmunk:make-cp-vect dfloat-x dfloat-y)))
     player))
-
-(defmethod select-weapon ((player james) weapon-class-name)
-  (let* ((tr (find-component player 'transform-c))
-         (children (children tr)))
-    (with-slots (weapon) player
-      (let ((child-weapon (find-if
-                           (lambda (child) (typep child weapon-class-name))
-                           children)))
-        (if child-weapon
-            (setf weapon child-weapon)
-            (error (format nil "Weapon ~A isn't a child of the player" weapon-class-name)))))))
-
-(defmethod is-invincible ((player james))
-  (with-slots (last-hit-time invinc-seconds) player
-    (not (local-time:timestamp> (local-time:now) (local-time:timestamp+ last-hit-time invinc-seconds :sec)))))
-
-(defmethod hurt ((player james) (ch weapon-charge-type))
-  (with-slots (health last-hit-time invinc-seconds) player
-    (if (not (is-invincible player))
-        (progn
-          (setf last-hit-time (local-time:now))
-          (with-slots (damage-range) ch
-            (let ((damage (+ (car damage-range) (random (cdr damage-range)))))
-              (setf health (- health damage))
-              (if (<= health 0)
-                  ;; Restart the game
-                  ;; FIXME: this is kinda ugly
-                  (push-event :restart-current-state))))))))
